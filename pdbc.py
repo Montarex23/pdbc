f42aefc00c8d21dc20aa8dd12c134f87b8cc4d47
Nimplex <39964594+Nimplex@users.noreply.github.com>
2019-12-10 22:20:25 +0100

    Add files via upload

--- pdbc.py
+++ pdbc.py
@@ -6,10 +6,7 @@ import requests
 import subprocess
 import os
 import time
-
-with open("languages.json", "r+") as languages_file:
-    lang = json.loads(languages_file.read())
-    lg = lang["lang"]
+import pyperclip as pyp
 
 bwd = os.getcwd()
         
@@ -21,7 +18,7 @@ def newbot(name):
     variables_file.write("{}")
     variables_file.close()
     config_file = open("config.json", "w")
-    config_file.write('{"token":"", "prefix":"!", "ownerid":"1"}')
+    config_file.write('{"token":"", "prefix":"!", "ownerid":"0"}')
     config_file.close()
     with open("config.json", "r") as config_file:
         global config
@@ -54,8 +51,6 @@ def editjson(file, key, value):
         openedfile.seek(0)
         json.dump(data, openedfile)
         openedfile.close()
-    if key == "lang":
-         messagebox.showwarning(lang[value]["warning"], lang[value]["msgboxlang"])
 
 
                                                                     ############
@@ -102,7 +97,27 @@ class Main(tk.Tk):
                                                                     ### HOME PAGE ###
                                                                     #################
 
-        
+def copy(data):
+    pyp.copy(data)
+    pyp.paste()
+def popup(text,typ):
+    pop = tk.Tk()
+    pop.title("Popup")
+    pop.resizable(width=False, height=False)
+    pop.geometry("200x100")
+    if typ == "text":
+        lbl = tk.Label(pop, text=text)
+        lbl.pack()
+        btn = tk.Button(pop, text="Ok",command= lambda: pop.destroy())
+        btn.pack()
+    if typ == "link":
+        lbl = tk.Label(pop, text=text)
+        lbl.pack()
+        btn = tk.Button(pop, text="Ok",command= lambda: pop.destroy())
+        btn.pack()
+        btn = tk.Button(pop, text="Copy",command= lambda: copy(text))
+        btn.pack()
+    
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -110,25 +125,21 @@ class StartPage(tk.Frame):
         controller.title("Python Discord Bot Creator")
         controller.geometry("700x500")
         
-        tk.Label(self, text=lang[lg]["homepage"], font=LARGE_FONT).grid(row=0, column=0)
+        tk.Label(self, text="Strona główna", font=LARGE_FONT).grid(row=0, column=0)
 
-        tk.Label(self, text=lang[lg]["botname"]).grid(row=1, column=0, pady=30)
+        tk.Label(self, text="Nazwa: ").grid(row=1, column=0, pady=30)
         self.ent_botname = tk.Entry(self)
         self.ent_botname.grid(row=1, column=1)
-        btn_editbot = tk.Button(self, text=lang[lg]["editbot"], command=lambda: clseditbot(self, controller))
+        btn_editbot = tk.Button(self, text="Edytuj Bota", command=lambda: clseditbot(self, controller))
         btn_editbot.grid(row=1, column=2, padx=20)
-        btn_newbot = tk.Button(self, text=lang[lg]["newbot"], command=lambda: clsnewbot(self, controller))
+        btn_newbot = tk.Button(self, text="Nowy bot", command=lambda: clsnewbot(self, controller))
         btn_newbot.grid(row=1, column=3, padx=20)
 
-        btn_options = tk.Button(self, text=lang[lg]["options"], command=lambda: controller.show_frame(OptionsPage))
+        btn_options = tk.Button(self, text="Ustawienia", command=lambda: controller.show_frame(OptionsPage))
         btn_options.grid(row=2, column=0, pady=30)
 
-        tk.Label(self, text="Developer: Montarex23#3653").grid(row=3, pady=30)
-        tk.Label(self, text="Discord Server: https://discord.gg/NKumSM4").grid(row=4)
-
         tk.Label(self, text="0.0.2").grid(row=5, pady=50)
-
-
+        
         def clsnewbot(self, controller):
             StartPage.name = self.ent_botname.get()
             if StartPage.name != "":
@@ -137,9 +148,9 @@ class StartPage(tk.Frame):
                     newbot(StartPage.name)
                     controller.show_frame(BotEditor)
                 else:
-                    messagebox.showerror(lang[lg]["error"], lang[lg]["alreadyexists"])
+                    messagebox.showerror("Błąd", "alreadyexists")
             else:
-                messagebox.showerror(lang[lg]["error"], lang[lg]["emptyname"])
+                messagebox.showerror("Błąd", "Nazwa bota nie może być pusta")
 
         def clseditbot(self, controller):
             StartPage.name = self.ent_botname.get()
@@ -149,22 +160,16 @@ class StartPage(tk.Frame):
                     editbot(StartPage.name)
                     controller.show_frame(BotEditor)
                 else:
-                    messagebox.showerror(lang[lg]["error"], lang[lg]["notexists"])
+                    messagebox.showerror("Błąd", "Taki bot nie istnieje")
             else:
-                messagebox.showerror(lang[lg]["error"], lang[lg]["emptyname"])
+                messagebox.showerror("Błąd", "Nazwa bota nie może być pusta")
 
     def menubar(self):
         self.menu = tk.Menu(self) 
         cascade = tk.Menu(self.menu)
         self.menu.add_cascade(label="Program", menu = cascade)        
-        cascade.add_command(label = "opcja 1", command=lambda: print("xD"))
-        cascade.add_command(label = "opcja 2", command=lambda: print("xD"))       
-        cascade2 = tk.Menu(cascade, tearoff = 0)
-        cascade.add_cascade(label = "Podmenu", menu = cascade2)       
-        cascade2.add_radiobutton(label = "opcja 1", command=lambda: print("xD"))
-        cascade2.add_radiobutton(label = "opcja 2", command=lambda: print("xD"))
-        cascade2.add_separator()
-        cascade2.add_checkbutton(label = "opcja 3", command=lambda: print("xD"))
+        cascade.add_command(label = "Developer", command=lambda: popup("Montarex23#3653", "text"))
+        cascade.add_command(label = "Discord server", command=lambda: popup("https://discord.gg/NKumSM4", "link"))
         return self.menu
 
 
@@ -192,7 +197,7 @@ class BotEditor(tk.Frame):
 
         self.lb_icmd = tk.Label(self)
         self.lb_icmd.grid(row=1)
-        self.lb_icmd.configure(text=lang[lg]["loading"])
+        self.lb_icmd.configure(text="Ładowanie...")
 
         tk.Label(self, text="-" * 120).grid(row=2, column=0, columnspan=4)
 
@@ -204,34 +209,35 @@ class BotEditor(tk.Frame):
         self.ent_prefix = tk.Entry(self, width=15)
         self.ent_prefix.grid(row=4, column=1)
         self.ent_prefix.delete(0,"end")
+
         try:
             self.ent_prefix.insert(0,config["prefix"])
         except:
-            self.ent_prefix.insert(0,lang[lg]["loading"])
-        lb_ownerid = tk.Label(self, text=lang[lg]["ownerid"]).grid(row=5, column=0)
+            self.ent_prefix.insert(0,"Ładowanie...")
+        lb_ownerid = tk.Label(self, text="ID Właściciela: ").grid(row=5, column=0)
         self.ent_ownerid = tk.Entry(self, width=30)
         self.ent_ownerid.grid(row=5, column=1)
         self.ent_ownerid.delete(0,"end")
         try:
             self.ent_ownerid.insert(0,config["ownerid"])
         except:
-            self.ent_prefix.insert(0,lang[lg]["loading"])
-        btn_save = tk.Button(self, text=lang[lg]["save"], command=lambda: self.save_config(), width=10).grid(row=6)
+            self.ent_prefix.insert(0,"Ładowanie...")
+        btn_save = tk.Button(self, text="Zapisz", command=lambda: self.save_config(), width=10).grid(row=6)
 
         tk.Label(self, text="-" * 120).grid(row=7, column=0, columnspan=4)
 
-        tk.Label(self, text=lang[lg]["cmdnametoedit"]).grid(row=8, column=0)
+        tk.Label(self, text="Nazwa komendy do edytowania: ").grid(row=8, column=0)
         self.ent_cmdtoedit = tk.Entry(self, width=40)
         self.ent_cmdtoedit.grid(row=8, column=1)
-        tk.Button(self, text=lang[lg]["editcmd"], command=lambda: editcmd(self, controller, self.ent_cmdtoedit.get())).grid(row=8,column=2)
+        tk.Button(self, text="Edytuj komende", command=lambda: editcmd(self, controller, self.ent_cmdtoedit.get())).grid(row=8,column=2)
 
-        btn_newcmd = tk.Button(self, text=lang[lg]["newcmd"], command=lambda: controller.show_frame(CommandEditor)).grid(row=9,pady=10)
+        btn_newcmd = tk.Button(self, text="Nowa komenda", command=lambda: controller.show_frame(CommandEditor)).grid(row=9,pady=10)
 
         tk.Label(self, text="-" * 120).grid(row=10, column=0, columnspan=4)
         
-        btn_runbot = tk.Button(self, text=lang[lg]["runbot"], command=lambda: runbot(self, StartPage.name)).grid(row=11)
+        btn_runbot = tk.Button(self, text="Uruchom bota", command=lambda: runbot(self, StartPage.name)).grid(row=11)
 
-        btn_backtohome = tk.Button(self, text=lang[lg]["backtohome"], command=lambda: controller.show_frame(StartPage))
+        btn_backtohome = tk.Button(self, text="Wróć do głównej", command=lambda: controller.show_frame(StartPage))
         btn_backtohome.grid(row=12, pady=100)
 
         self.update_frame()
@@ -253,8 +259,8 @@ class BotEditor(tk.Frame):
         try:
             i = len(cmdsinfo.keys())
         except:
-            i = lang[lg]["loading"]
-        self.lb_icmd.configure(text=f"{lang[lg]['cmds']}: {i}")
+            i = "Ładowanie..."
+        self.lb_icmd.configure(text=f"Komendy: {i}")
         self.lb_icmd.after(1000, self.update_frame)
         global repeat
         try:
@@ -268,14 +274,14 @@ class BotEditor(tk.Frame):
                 self.ent_prefix.insert(0,config["prefix"])
                 repeat = False
             except Exception as e:
-                self.ent_prefix.insert(0,lang[lg]["loading"])
+                self.ent_prefix.insert(0,"Ładowanie...")
 
             self.ent_ownerid.delete(0,"end")
             try:
                 self.ent_ownerid.insert(0,config["ownerid"])
                 repeat = False
             except Exception as e:
-                self.ent_ownerid.insert(0,lang[lg]["loading"])
+                self.ent_ownerid.insert(0,"Ładowanie...")
 
     def save_config(self):
         if self.ent_token.get() != "":
@@ -288,7 +294,7 @@ class BotEditor(tk.Frame):
     def menubar(self):
         self.menu = tk.Menu(self) 
         cmdsmenu = tk.Menu(self.menu)
-        self.menu.add_cascade(label=lang[lg]["cmds"], menu = cmdsmenu)
+        self.menu.add_cascade(label="Komendy", menu = cmdsmenu)
         for key in cmdsinfo.keys():
             cmdsmenu.add_command(label = str(key).split('.')[0], command=lambda: print("xD"))
         return self.menu
@@ -302,32 +308,23 @@ class OptionsPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text=lang[lg]["options"], font=LARGE_FONT)
+        label = tk.Label(self, text="Ustawienia", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
 
-        button1 = tk.Button(self, text=lang[lg]["backtohome"], command=lambda: controller.show_frame(StartPage))
+        button1 = tk.Button(self, text="Wróć do głównej", command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
         rb_var = tk.StringVar()
-        rb_female = tk.Radiobutton(self, variable = rb_var, value = "en", text = "English", command=lambda: editjson("languages", "lang", "en"))
-        rb_male = tk.Radiobutton(self, variable = rb_var, value = "pl", text = "Polski", command=lambda: editjson("languages", "lang", "pl"))
-        rb_var.set(lang["lang"])
-        
-        rb_female.pack()
+        rb_male = tk.Radiobutton(self, variable = rb_var, value = "pl", text = "Polski", command=lambda: popup("haha", "text"))
+
         rb_male.pack()
 
     def menubar(self):
         self.menu = tk.Menu(self) 
         cascade = tk.Menu(self.menu)
         self.menu.add_cascade(label="Program", menu = cascade)        
-        cascade.add_command(label = "opcja 1", command=lambda: print("xD"))
-        cascade.add_command(label = "opcja 2", command=lambda: print("xD"))       
-        cascade2 = tk.Menu(cascade, tearoff = 0)
-        cascade.add_cascade(label = "Podmenu", menu = cascade2)       
-        cascade2.add_radiobutton(label = "opcja 1", command=lambda: print("xD"))
-        cascade2.add_radiobutton(label = "opcja 2", command=lambda: print("xD"))
-        cascade2.add_separator()
-        cascade2.add_checkbutton(label = "opcja 3", command=lambda: print("xD"))
+        cascade.add_command(label = "Developer", command=lambda: popup("Montarex23#3653", "text"))
+        cascade.add_command(label = "Discord server", command=lambda: popup("https://discord.gg/NKumSM4", "link"))  
         return self.menu
 
 
@@ -340,58 +337,44 @@ class CommandEditor(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
-        tk.Label(self, text=lang[lg]["cmdeditor"], font=LARGE_FONT).grid(row=0, column=1)
-        tk.Label(self, text=lang[lg]["cmdname"]).grid(row=1, column=0)
+        tk.Label(self, text="Edytor komend", font=LARGE_FONT).grid(row=0, column=1)
+        tk.Label(self, text="Nazwa komendy").grid(row=1, column=0)
         self.ent_cmdname = tk.Entry(self, width=40)
         self.ent_cmdname.grid(row=3, column=0)
 
-        tk.Label(self, text=lang[lg]["cmdoutput"]).grid(row=4, column=0)
+        tk.Label(self, text="Output komendy").grid(row=4, column=0)
         self.tb_output = tk.Text(self, width = 80, height = 30)
         self.tb_output.grid(row=5, column=0)
         
-        tk.Label(self, text=lang[lg]["actions"]).grid(row=2, column=2)
+        tk.Label(self, text="* - Opcjonalne Akcje:").grid(row=2, column=2)
         self.treeview = ttk.Treeview(self)
+        send = ["Wysyła wiadomośc", "tekst - Tekst wiadomości"]
+        createChannel = ["Tworzy kanał", "typ - voice/text", "nazwa - nazwa kanału"]
+        script = ["DLA ZAAWANSOWANYCH", "Evaluje linijke","linijka - linijka pythona"]
+        deleteMessage = ["Usuwa wiadomość", "msg - usermsg/botmsg/id", "  usermsg - komenda", "  botmsg - ostatnia wiad. bota", "  id - id wiadomości"]
+        log = ["Zapisuje dany tekst", "tekst - Tekst"]
+        args = ["Argumenty z wiadomości", "arg - numer argumentu", "var - Nazwa zmiennej"] #todo
+        status = ["Ustawia status bota", "nazwa - text wyświetlany w statusie bota", "typ - Typ statusu", "watching","playing","listening"]
 
-        if lg == "pl":
-            send = ["Wysyła wiadomośc", "tekst - Tekst wiadomości"]
-            createChannel = ["Tworzy kanał", "typ - voice/text", "nazwa - nazwa kanału"]
-            script = ["DLA ZAAWANSOWANYCH", "Evaluje linijke","linijka - linijka pythona"]
-            deleteMessage = ["Usuwa wiadomość", "msg - usermsg/botmsg/id", "  usermsg - komenda", "  botmsg - ostatnia wiad. bota", "  id - id wiadomości"]
-            log = ["Zapisuje dany tekst", "tekst - Tekst"]
-            args = ["Argumenty z wiadomości", "arg - numer argumentu", "var - Nazwa zmiennej"] #todo
         
-            self.add_values(send, ".send:tekst")
-            self.add_values(deleteMessage, ".deleteMessage:msg")
-            self.add_values(log, ".log:tekst")
-            self.add_values(createChannel, ".createChannel:typ, nazwa")
-            #self.add_values(args, ".args:arg = var")
-            self.add_values(script, ".script:linijka")
+        self.add_values(send, ".send:tekst")
+        self.add_values(deleteMessage, ".deleteMessage:msg")
+        self.add_values(log, ".log:tekst")
+        self.add_values(createChannel, ".createChannel:typ, nazwa")
+        #self.add_values(args, ".args:arg = var")
+        self.add_values(script, ".script:linijka")
+        self.add_values(status, ".status:nazwa, typ")
 
-        elif lg == "en":
-            send = ["Sends message", "text - Message text"]
-            createChannel = ["Creates channel", "type - voice/text", "name - channel name"]
-            script = ["ADVANCED", "Evals line","line - python line"]
-            deleteMessage = ["Deletes message", "msg - usermsg/botmsg/id", "  usermsg - command", "  botmsg - last bot message", "  id - message id"]
-            log = ["Logs text", "text - text"]
-            args = ["Command arguments", "arg - argument number", "var - variable name"] #todo
-        
-            self.add_values(send, ".send:text")
-            self.add_values(deleteMessage, ".deleteMessage:msg")
-            self.add_values(log, ".log:text")
-            self.add_values(createChannel, ".createChannel:type, name")
-            #self.add_values(args, ".args:arg = var")
-            self.add_values(script, ".script:line")
-        
         self.sb_treeview = tk.Scrollbar(self)
         self.treeview.config(yscrollcommand = self.sb_treeview.set)
         self.sb_treeview.config(command = self.treeview.yview)
         self.sb_treeview.place(in_ = self.treeview, relx = 1., y = 0, relheight = 1.)
         self.treeview.grid(row=3, column=2, rowspan=3)
 
-        btn_create = tk.Button(self, text=lang[lg]["create/edit"], width=20, command=lambda:createbutton())
+        btn_create = tk.Button(self, text="Stwórz/edytuj", width=20, command=lambda:createbutton())
         btn_create.grid(row=6, column=1, pady=20)
 
-        btn_back = tk.Button(self, text=lang[lg]["back"], width=20, command=lambda:controller.show_frame(BotEditor))
+        btn_back = tk.Button(self, text="Wróć", width=20, command=lambda:controller.show_frame(BotEditor))
         btn_back.grid(row=7, column=1, pady=30)
 
         def createbutton():
@@ -435,17 +418,10 @@ class CommandEditor(tk.Frame):
     def menubar(self):
         self.menu = tk.Menu(self) 
         cascade = tk.Menu(self.menu)
-        self.menu.add_cascade(label="Program", menu = cascade)
-        cascade.add_command(label = "opcja 1", command=lambda: print("xD"))
-        cascade.add_command(label = "opcja 2", command=lambda: print("xD"))       
-        cascade2 = tk.Menu(cascade, tearoff = 0)
-        cascade.add_cascade(label = "Podmenu", menu = cascade2)       
-        cascade2.add_radiobutton(label = "opcja 1", command=lambda: print("xD"))
-        cascade2.add_radiobutton(label = "opcja 2", command=lambda: print("xD"))
-        cascade2.add_separator()
-        cascade2.add_checkbutton(label = "opcja 3", command=lambda: print("xD"))
+        self.menu.add_cascade(label="Program", menu = cascade)        
+        cascade.add_command(label = "Developer", command=lambda: popup("Montarex23#3653", "text"))
+        cascade.add_command(label = "Discord server", command=lambda: popup("https://discord.gg/NKumSM4", "link"))  
         return self.menu
-
 
 
 app = Main()
